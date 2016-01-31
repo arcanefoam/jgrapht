8331df85ddef36b9a13f7f2cfecd0bec88a0e462
Horacio Hoyos <arcanefoam@gmail.com>
2016-01-31 18:26:15 +0000

    Modified the test to adjust to new functionality

--- jgrapht-core/src/test/java/org/jgrapht/graph/SimpleAdjunctGraphTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/graph/SimpleAdjunctGraphTest.java
@@ -97,13 +97,15 @@ public class SimpleAdjunctGraphTest
     public void testProtectedBase() {
 
         adjunct.removeVertex(v1);
-        assertTrue(adjunct.containsVertex(v1));
+        assertFalse(adjunct.containsVertex(v1));
+        assertTrue(primary.containsVertex(v1));
         DefaultEdge e1 = adjunct.getEdge(v1, v2);
         assertNotNull(e1);
         adjunct.removeEdge(e1);
-        assertTrue(adjunct.containsEdge(e1));
+        assertFalse(adjunct.containsEdge(e1));
         adjunct.removeEdge(v1, v2);
-        assertTrue(adjunct.containsEdge(e1));
+        assertFalse(adjunct.containsEdge(e1));
+        assertTrue(primary.containsEdge(e1));
     }
 
     @Test
